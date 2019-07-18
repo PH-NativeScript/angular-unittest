a2090afbb129a2052d4431bdcf79a178510c5475
PH-Nativescript <info@qwert.dk>
2019-07-18 15:44:55 +0200

    Part II completed (hopefully)

--- src/app/services/dice.service.spec.ts
+++ src/app/services/dice.service.spec.ts
@@ -1,9 +1,68 @@
+import { DiceService } from './dice.service';
+
 describe('DiceService', () => {
-  it('should have a score of 0 when just initialized', () => {});
-  it('should generate a number between 1 and 6 when throwDice is called', () => {});
-  it('should return "Not so good" when throw is 1', () => {});
-  it('should return "Well" when throw is 3', () => {});
-  it('should return "Brilliant" when throw is 5', () => {});
-  it('should accumulate values send to the scoreAdd method', () => {});
-  it('should reset score to 0 when cleared', () => {});
+  it('should have a score of 0 when just initialized', () => {
+    const diceService = new DiceService();
+
+    expect(diceService.score).toEqual(0);
+  });
+
+  it('should generate a number between 1 and 6 when throwDice is called', () => {
+    const diceService = new DiceService();
+
+    const throwValue = diceService.throwDice();
+
+    expect(throwValue).toBeGreaterThanOrEqual(1);
+    expect(throwValue).toBeLessThanOrEqual(6);
+  });
+
+  it('should return "Not so good" when throw is 1', () => {
+    const diceService = new DiceService();
+
+    const throwValue = 1;
+    const message = diceService.message(throwValue);
+
+    expect(message).toEqual('1 - Not so good');
+  });
+
+  it('should return "Well" when throw is 3', () => {
+    const diceService = new DiceService();
+
+    const throwValue = 3;
+    const message = diceService.message(throwValue);
+
+    expect(message).toEqual('3 - Well');
+  });
+
+  it('should return "Brilliant" when throw is 5', () => {
+    const diceService = new DiceService();
+
+    const throwValue = 5;
+    const message = diceService.message(throwValue);
+
+    expect(message).toEqual('5 - Brilliant');
+  });
+
+  it('should accumulate values send to the scoreAdd method', () => {
+    const diceService = new DiceService();
+    const throwValue1 = 5;
+    const throwValue2 = 2;
+
+    diceService.scoreAdd(throwValue1);
+    diceService.scoreAdd(throwValue2);
+    const actual = diceService.score;
+    const expected = throwValue1 + throwValue2;
+
+    expect(actual).toEqual(expected);
+  });
+
+  it('should reset score to 0 when cleared', () => {
+    const diceService = new DiceService();
+
+    diceService.scoreAdd(5);
+    diceService.clear();
+    const actual = diceService.score;
+
+    expect(actual).toEqual(0);
+  });
 });
